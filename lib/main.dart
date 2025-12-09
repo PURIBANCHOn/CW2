26ba55f2d0ae77f3715edb00b481bd5eb839400d
PURIBANCHOn <s6706022510298@email.kmutnb.ac.th>
2025-12-09 11:46:37 +0700

    CW2.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Myapp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "CW2",
-      home: const MyHomePage(),
-    );
+    return MaterialApp(title: "CW2", home: MyHomePage());
   }
 }
 
@@ -25,295 +22,247 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int num = 0;
-  String num1=" ";
-  String num2="0";
-  String Operater=" ";
-  String ShowVal="0";
-  double result = 0;
+  String num1 = "";
+  String num2 = "";
+  String operator = "";
+  String Showval = "0";
+  double result = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Calculator")),
+      appBar: AppBar(title: Text("AppBar")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(ShowVal,style: TextStyle(fontSize: 40),
-            ),
-            const SizedBox(height: 20),
+            Text(Showval, style: TextStyle(fontSize: 40)),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FloatingActionButton(
                   onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "7";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "7";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "7",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("7", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
-                    onPressed: () {
+                  onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "8";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "8";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "8",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("8", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
-                   onPressed: () {
+                  onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "9";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "9";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "9",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("9", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
                   onPressed: () {
-                    Operater = "+";
+                    operator = "+";
                   },
-                  child: const Text(
-                    "+",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("+", style: TextStyle(fontSize: 30)),
                 ),
               ],
             ),
-            SizedBox(height: 10,),
-             Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FloatingActionButton(
-                   onPressed: () {
+                  onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "4";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "4";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "4",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("4", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
-                   onPressed: () {
+                  onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "5";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "5";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "5",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("5", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
-                   onPressed: () {
+                  onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "6";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "6";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "6",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("6", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
                   onPressed: () {
-                    Operater = "-";
+                    operator = "-";
                   },
-                  child: const Text(
-                    "-",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("-", style: TextStyle(fontSize: 30)),
                 ),
               ],
             ),
-            SizedBox(height: 10,),
-             Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FloatingActionButton(
-                   onPressed: () {
+                  onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "1";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "1";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "1",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("1", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
-                FloatingActionButton(
-                   onPressed: () {
-                    setState(() {
-                      if (Operater.isEmpty){
-                        num1 += "2";
-                        ShowVal = num1;
-                      } else {
-                        num2 += "2";
-                        ShowVal = num2;
-                      }
-                    });
-                  },
-                  child: const Text(
-                    "2",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                SizedBox(width: 20),
-                FloatingActionButton(
-                   onPressed: () {
-                    setState(() {
-                      if (Operater.isEmpty){
-                        num1 += "3";
-                        ShowVal = num1;
-                      } else {
-                        num2 += "3";
-                        ShowVal = num2;
-                      }
-                    });
-                  },
-                  child: const Text(
-                    "3",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
                   onPressed: () {
-                    Operater = "*";
+                    setState(() {
+                      if (operator.isEmpty) {
+                        num1 += "2";
+                        Showval = num1;
+                      } else {
+                        num2 += "2";
+                        Showval = num2;
+                      }
+                    });
                   },
-                  child: const Text(
-                    "*",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("2", style: TextStyle(fontSize: 30)),
+                ),
+                SizedBox(height: 10),
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      if (operator.isEmpty) {
+                        num1 += "3";
+                        Showval = num1;
+                      } else {
+                        num2 += "3";
+                        Showval = num2;
+                      }
+                    });
+                  },
+                  child: Text("3", style: TextStyle(fontSize: 30)),
+                ),
+                SizedBox(height: 10),
+                FloatingActionButton(
+                  onPressed: () {
+                    operator = "*";
+                  },
+                  child: Text("*", style: TextStyle(fontSize: 30)),
                 ),
               ],
             ),
-            SizedBox(height: 10,),
-             Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FloatingActionButton(
-                   onPressed: () {
+                  onPressed: () {
                     setState(() {
-                      if (Operater.isEmpty){
+                      if (operator.isEmpty) {
                         num1 += "0";
-                        ShowVal = num1;
+                        Showval = num1;
                       } else {
                         num2 += "0";
-                        ShowVal = num2;
+                        Showval = num2;
                       }
                     });
                   },
-                  child: const Text(
-                    "0",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("0", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
                   onPressed: () {
                     setState(() {
-                      num1 += " ";
-                      num2 += " ";
-                      Operater = " ";
-                      ShowVal = "0";
+                      num1 = "";
+                      num2 = "";
+                      operator = "";
+                      Showval = "0";
                     });
                   },
-                  child: const Text(
-                    "C",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("C", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
                   onPressed: () {
                     setState(() {
-                    if (num1 == " " || num2 == " " || Operater == " ")return;
-                    if (Operater == "+"){
-                      result = (double.parse(num1) + double.parse(num2));
-                    } else if (Operater == "-"){
-                      result = (double.parse(num1) - double.parse(num2));
-                    } else if (Operater == "*"){
-                      result = (double.parse(num1) * double.parse(num2));
-                    } else if (Operater == "/"){
-                      result = (double.parse(num1) / double.parse(num2));
-                    }
-                    ShowVal = result.toString();
+                      if (num1 == " " || num2 == "") {
+                        return;
+                      }
+                      if (operator == "+") {
+                        result = double.parse(num1) + double.parse(num2);
+                      } else if (operator == "-") {
+                        result = double.parse(num1) - double.parse(num2);
+                      } else if (operator == "*") {
+                        result = double.parse(num1) * double.parse(num2);
+                      } else if (operator == "/") {
+                        result = double.parse(num1) / double.parse(num2);
+                      }
+                      Showval = result.toString();
+                      num1 = result.toString();
+                      num2 = "";
+                      operator = "";
                     });
                   },
-                  child: const Text(
-                    "=",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("=", style: TextStyle(fontSize: 30)),
                 ),
-                SizedBox(width: 20),
+                SizedBox(height: 10),
                 FloatingActionButton(
                   onPressed: () {
-                    Operater = "/";
+                    operator = "/";
                   },
-                  child: const Text(
-                    "/",
-                    style: TextStyle(fontSize: 30),
-                  ),
+                  child: Text("/", style: TextStyle(fontSize: 30)),
                 ),
               ],
             ),
